d0c949e4a484a34dc48f56283d68763f60723b99
Emanuel Danciu <emanuel.danciu@nxp.com>
2024-09-27 16:30:48 +0300

    this is my forth comit

--- first_file.c
+++ first_file.c
@@ -1,1 +1,5 @@
-"this is my first line of code added to this file" <i use this file as a test>
+"this is my first line of code added to this file" <this is after i added the key to github> 
+
+added stuff
+
+added stuff
