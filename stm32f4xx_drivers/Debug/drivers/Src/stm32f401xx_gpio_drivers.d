8715fc394a6e76145c7309d545f8716809f67ff5
emanueldanciu11 <123165850+emanueldanciu11@users.noreply.github.com>
2024-10-01 20:49:07 +0300

    Delete stm32f4xx_drivers directory

--- stm32f4xx_drivers/Debug/drivers/Src/stm32f401xx_gpio_drivers.d
+++
@@ -1,6 +0,0 @@
-drivers/Src/stm32f401xx_gpio_drivers.o: \
- ../drivers/Src/stm32f401xx_gpio_drivers.c \
- C:/Users/Emi/STM32CubeIDE/workspace_1.16.0/stm32f4xx_drivers/drivers/Inc/stm32f401xx_gpio_drivers.h \
- C:/Users/Emi/STM32CubeIDE/workspace_1.16.0/stm32f4xx_drivers/drivers/Inc/stm32f401xx.h
-C:/Users/Emi/STM32CubeIDE/workspace_1.16.0/stm32f4xx_drivers/drivers/Inc/stm32f401xx_gpio_drivers.h:
-C:/Users/Emi/STM32CubeIDE/workspace_1.16.0/stm32f4xx_drivers/drivers/Inc/stm32f401xx.h:
